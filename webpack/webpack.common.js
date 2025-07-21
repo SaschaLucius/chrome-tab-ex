d98a0f5fc89836309b2280aaf6d81d19ce655066
Sascha Lucius <sascha.lucius@staffbase.com>
2025-07-21 16:42:33 +0200

    cleanup

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,36 +4,36 @@ const CopyPlugin = require("copy-webpack-plugin");
 const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
-    entry: {
-      gt: path.join(srcDir, 'groupTabs.ts'),
+  entry: {
+    gt: path.join(srcDir, "groupTabs.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist/js"),
+    filename: "[name].js",
+  },
+  devtool: false, // Disable eval() for Chrome extension CSP compliance
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial",
     },
-    output: {
-        path: path.join(__dirname, "../dist/js"),
-        filename: "[name].js",
-    },
-    devtool: false, // Disable eval() for Chrome extension CSP compliance
-    optimization: {
-        splitChunks: {
-            name: "vendor",
-            chunks: "initial",
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: "../", context: "public" }],
+      options: {},
+    }),
+  ],
 };
